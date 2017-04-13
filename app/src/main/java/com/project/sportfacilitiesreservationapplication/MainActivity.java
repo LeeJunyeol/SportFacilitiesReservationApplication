6b3f186b2335dac37405d6c9040d00e173734b8d
LeeJunyeol <chljy33@naver.com>
2017-04-13 22:51:22 +0900

    툴바 프래그먼트 제거 -> 일반적인 툴바 이용
    메인에 Tab 추가.
    Tab1(체육시설), Tab3(즐겨찾기) : 준열
    Tab2(상대찾기): 문식
    
    fragment tab1, tab2, tab3 레이아웃 설정하고
    내부 컨텐츠 기능 액티비티 작성해야 함.
    
    네비게이션 드로워, 설정: 주호
    글쓰기, 상대구하기, 팀구하기: 태중이형

--- app/src/main/java/com/project/sportfacilitiesreservationapplication/MainActivity.java
+++ app/src/main/java/com/project/sportfacilitiesreservationapplication/MainActivity.java
@@ -1,7 +1,12 @@
 package com.project.sportfacilitiesreservationapplication;
 
+import android.app.Fragment;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,53 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // activity_main에 있는 툴바를 액션바로 이용
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tb_common);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout_main);
+
+        tabLayout.addTab(tabLayout.newTab().setText("체육시설"));
+        tabLayout.addTab(tabLayout.newTab().setText("상대찾기"));
+        tabLayout.addTab(tabLayout.newTab().setText("즐겨찾기"));
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        final PagerAdapter adapter = new TabPagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener(){
+            /**
+             * Called when a tab enters the selected state.
+             *
+             * @param tab The tab that was selected
+             */
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            /**
+             * Called when a tab exits the selected state.
+             *
+             * @param tab The tab that was unselected
+             */
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            /**
+             * Called when a tab that is already selected is chosen again by the user. Some applications
+             * may use this action to return to the top level of a category.
+             *
+             * @param tab The tab that was reselected.
+             */
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/project/sportfacilitiesreservationapplication/TabPagerAdapter.java
@@ -0,0 +1,48 @@
+package com.project.sportfacilitiesreservationapplication;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by jylee on 2017-04-13.
+ */
+
+public class TabPagerAdapter extends FragmentPagerAdapter{
+    /**
+     * Return the Fragment associated with a specified position.
+     *
+     * @param position
+     */
+    int tabCount;
+
+    public TabPagerAdapter(FragmentManager fm, int numberOfTabs) {
+        super(fm);
+        this.tabCount = numberOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                Tab1SportFacListFragment tab1SportFacListFragment = new Tab1SportFacListFragment();
+                return tab1SportFacListFragment;
+            case 1:
+                Tab2FindOpponentFragment tab2FindOpponentFragment = new Tab2FindOpponentFragment();
+                return tab2FindOpponentFragment;
+            case 2:
+                Tab3FavoriteSprotFacFragment tab3FavoriteSprotFacFragment = new Tab3FavoriteSprotFacFragment();
+                return tab3FavoriteSprotFacFragment;
+            default:
+                return null;
+        }
+    }
+
+    /**
+     * Return the number of views available.
+     */
+    @Override
+    public int getCount() {
+        return tabCount;
+    }
+}
